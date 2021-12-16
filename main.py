322398d258ccb7764a1bfc70b1f1e017244529dd
Rene Vrijenhoek <info@vortex-ict.nl>
2021-12-16 09:19:32 +0100

    added main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title('first test with streamlit, git and python')
+st.subheader('This is a subheader')
+st.write('''This is a first test with the framework streamlit.io and python. Just for deploying python web apps.
+write code in editor, push and automated publish. Is realtime code updates also possible?''')
